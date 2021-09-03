dccc31ada5cde3cf406fc46be39a8329859db579
adi <adi.sunjana@redtech.co.id>
2021-09-03 22:22:44 +0700

    update setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule , Routes } from '@angular/router';
+
+
+
+
+const routes : Routes = [
+    { path : '' , redirectTo : '/home' , pathMatch : 'full' },
+    { path : 'home' , loadChildren : ()=>import('./pages/home/home.module').then(m=>m.HomeModule) }
+]
+
+
+@NgModule({
+    declarations : [],
+    imports : [
+        RouterModule.forRoot(routes)
+    ]
+})
+
+export class appRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
