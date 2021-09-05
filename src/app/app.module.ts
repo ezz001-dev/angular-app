bf84fbc8d60c1aa9a9e3dcd8f3fe091f9935f508
adi <adi.sunjana@redtech.co.id>
2021-09-05 09:28:55 +0700

    add font awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { appRoutingModule } from './app-routing.module';
 
+
 @NgModule({
   declarations: [
     AppComponent,
