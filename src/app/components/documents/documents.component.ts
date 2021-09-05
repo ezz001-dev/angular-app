ecf9f023a851a0e32e167283b77fcd519a979e68
adi <adi.sunjana@redtech.co.id>
2021-09-05 20:20:05 +0700

    update all

--- src/app/components/documents/documents.component.ts
+++ src/app/components/documents/documents.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
 
 @Component({
   selector: 'app-documents',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DocumentsComponent implements OnInit {
+  @Input() highLight : any;
+  @Input() right : any;
+  @Input() bottom : any;
+  @Input() dataVideos : any;
 
   constructor() { }
 
--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -10,6 +10,15 @@ export class FooterComponent implements OnInit {
 
   constructor() { }
 
+  footerNav = [
+    { name : 'About' , slug : 'about' },
+    { name : 'For Bussines' , slug : 'for-bussines' },
+    { name : 'Suggestion' , slug : 'suggestion' },
+    { name : 'Help & FAQs' , slug : 'help-faq' },
+    { name : 'Contact' , slug : 'contact' },
+    { name : 'Pricing' , slug : 'pricing' },
+  ]
+
   ngOnInit(): void {
   }
 
